8a0b468f7d67b6bf5a2f32bb2277c9abc4f335e2
Alan Borges <alanborgesdev0@gmail.com>
2026-05-30 14:02:26 -0300

    feat: adiciona lista com 10 últimos repositórios

--- src/js/profileView.js
+++ src/js/profileView.js
@@ -1,9 +1,24 @@
-export function renderProfile(userData, container) {
-  container.innerHTML = `
+export function renderProfile(userData, userRepos, container) {
+    const repositoriesHTML = userRepos && userRepos.length> 0 ?
+    userRepos.map(repo => `
+        <a href="${repo.html_url}" target="_blank">
+            <div class="repository-card">
+                 <h3>${repo.name}</h3>
+                    <div class="repository-stats">
+                        <span>⭐ Stars ${repo.stargazers_count}</span>
+                        <span>🍴 Forks ${repo.forks_count}</span>
+                        <span>👀 Watchers ${repo.watchers_count}</span>
+                        <span>💻 Language ${repo.language_count || "Não informado"}</span>
+                    </div>
+            </div>
+        </a>
+        `).join("") : `<p>Nenhum repositório encontarado. </p>`;
+
+    container.innerHTML = `
     <div class="profile-card">
       <img src="${userData.avatar_url}" alt="Avatar de ${
-    userData.name
-  }" class="profile-avatar">
+          userData.name
+      }" class="profile-avatar">
       <div class="profile-info">
         <h2>${userData.name}</h2>
         <p>${userData.bio || "Não possui bio cadastrada 😢."}</p>
@@ -18,6 +33,13 @@ export function renderProfile(userData, container) {
         <div class="following">
             <h4>👥 Seguindo</h4>
             <span>${userData.following}</span>
+        </div>
+    </div>
+
+    <div class="profile-repositories">
+        <h2>Repositórios</h2>
+        <div class="repositories">
+            ${repositoriesHTML}
         </div>
     </div>
   `;
